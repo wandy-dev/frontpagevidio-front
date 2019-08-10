cbb6c582b21be8b06602981c377d0babc7ad7603
Andy DeVane <w.andrew.dev@gmail.com>
2019-08-09 20:30:48 -0400

    remove cast api url parameter

--- javascript/ytplayer.js
+++ javascript/ytplayer.js
@@ -1,6 +1,6 @@
 // Load the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api?enablecastapi=1" frameborder="0" allowfullscreen";
+tag.src = "https://www.youtube.com/iframe_api" frameborder="0" allowfullscreen";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
